282c6548b7b0a06cc24f22bb33cc0136f4ae943d
Siratul Islam <siratulislam@Siratuls-Air.Dlink>
2024-03-30 16:15:39 +0600

    resBooking

--- controllers/restaurant.js
+++ controllers/restaurant.js
@@ -2,21 +2,16 @@ const Restaurant = require("../models/Restaurant");
 
 exports.postRestaurant = async (req, res) => {
   try {
-    const { name, location, description, tag, image, price } = req.body;
+    const { name, location, description, tag, menu, number, image, price } = req.body;
 
     // Input validation - ensure required fields are present
-    if (!name || !location || !description || !tag || !image || !price) {
+    if (!name || !location || !description || !tag || !menu || !number || !image || !price) {
       return res.status(400).json({ message: "Missing required fields" });
     }
 
     // Create a new Restaurant object
     const newRestaurant = new Restaurant({
-      name,
-      location,
-      description,
-      tag,
-      image,
-      price
+      name, location, description, tag, menu, number, image, price
     });
 
     // Save the new restaurant to the database
